b95766ccfdd3447ca10c578110200df5a3fe7b71
Erick <erickperezquispe@gmail.com>
2018-07-17 16:58:57 -0500

    Entradas y salidas v2

--- resources/views/componentes/nav.blade.php
+++ resources/views/componentes/nav.blade.php
@@ -225,7 +225,7 @@
                 <nav class="sidebar-nav ">
                     <ul id="sidebarnav" class="p-t-30">
                         
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('inicio') }}" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Inicio</span></a></li> 
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('/') }}" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Inicio</span></a></li> 
                         
                         <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fas fa-archway"></i><span class="hide-menu">Almacen </span></a>
                             <ul aria-expanded="false" class="collapse  first-level">
